843e246350b657c568ae39efbe1d0164dca386e8
A-bdallah-mohamed <abddallahmohammed21@gmail.com>
2025-10-21 15:36:58 +0300

    parallax pc

--- home.js
+++ home.js
@@ -36,4 +36,47 @@ prevBtn.addEventListener("click", () => {
   updateClasses();
 });
 
+window.addEventListener("load", () => {
+  // Add "show" classes
+  document.getElementById("traveller").classList.add("show");
+  document.getElementById("plane1").classList.add("show");
+  document.getElementById("plane2").classList.add("show");
+  document.querySelector("header").classList.add("show");
+document.getElementById("smallheading").classList.add("show")
+document.getElementById("mainheading").classList.add("show")
+document.getElementById("paragraph").classList.add("show")
+document.getElementById("button1").classList.add("show")
+document.getElementById("button2").classList.add("show")
+document.querySelector("span").classList.add("showspan")
+
+
+
+  // Typing function
+  function type(i, full, component) {
+    if (i < full.length) {
+      component.textContent += full.charAt(i);
+      i++;
+      setTimeout(() => type(i, full, component), 30); // pass args again
+    }
+  }
+
+
+
+
+
+});
+
+
+function initParallaxScenes() {
+  const scenes = document.querySelectorAll('.scene');
+
+  scenes.forEach((scene) => {
+    new Parallax(scene);
+  });
+}
+
+// ✅ Run it
+initParallaxScenes();
+
+
 
